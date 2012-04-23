1d019bb6e0f83b887e2f4e5c721099c096d57ba7
Remco vanWilgen <remco_vanwilgen@hotmail.com>
2012-04-23 12:16:15 +0200

    login

--- mailsystem/lib/views/login.php
+++ mailsystem/lib/views/login.php
@@ -1,66 +1,48 @@
 
-	<div id="login">
-    
-		<?php
-        
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			
-			// Check of de gebruikersnaam en het wachtwoord overeenkomen met een database entry, zo ja: vul function met de functie van deze gebruiker
-			$result = $mailsystem->checkLogin($username, $password);
-			$function = $result[0]->function;
-			
-			// Als er nog niet op de login knop is geklikt, of als er wel op de login knop is gedrukt maar de fucntie variable is leeg
-            if (!$_POST['login'] || ( $_POST['login'] && !$function) ) {
-				
-				// Als er nog niet op de login knop is geklikt
-				if(!$_POST['login']) {
-				
-					echo "Vul hieronder je gebruikersnaam en wachtwoord in.";
-					
-				// Als er al wel op de login knop is geklikt
-				} else {
-				
-					echo "Gebruikersnaam en wachtwoord komen niet in de database voor.";
-					
-				}
-                        
-			?>
-    
-            <form action="index.php?view=login" method="post" />
-            
-                <label for="username">Gebruikersnaam</label>
-                <input type="text" id="username" name="username" value="<?php echo $_POST['username']; ?>" /><br />
-                
-                <label for="password">Wachtwoord</label>
-                <input type="password" id="password" name="password" value="<?php echo $_POST['password']; ?>" /><br />
-                
-                <input type="submit" name="login" value="Login" />
-    
-            </form>
-        
-        
-        <?php 
-		
-			// Als er wel op login is gedrukt en de functie var is gevuld
-			} else {
-                
-				if ($function == "editor") {
-			
-					echo "Je bent de redacteur.";
-                                        
-                                                                                         $editorSession = "true";
-                                                                                         $mailsystem->makeSession($editorSession);
-			
-				} else {
-				
-					echo "Je bent wel ingelogd, maar ik weet niet als wat!";
-				
-				}
+<div id="login">
+    <?php
 
-            }
-		
-		?>
-            
-    </div>
-    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+// Check of de gebruikersnaam en het wachtwoord overeenkomen met een database entry, zo ja: vul function met de functie van deze gebruiker
+    $result = $mailsystem->checkLogin($username, $password);
+    $function = $result[0]->function;
+
+// Als er nog niet op de login knop is geklikt, of als er wel op de login knop is gedrukt maar de fucntie variable is leeg
+    if (!$_POST['login'] || ( $_POST['login'] && !$function)) {
+
+// Als er nog niet op de login knop is geklikt
+        if (!$_POST['login']) {
+
+            echo "Vul hieronder je gebruikersnaam en wachtwoord in.";
+
+// Als er al wel op de login knop is geklikt
+        } else {
+
+            echo "Gebruikersnaam en wachtwoord komen niet in de database voor.";
+        }
+        ?>
+    <form action="index.php?view=login" method="post" />
+        <label for="username">Gebruikersnaam</label>
+        <input type="text" id="username" name="username" value="<?php echo $_POST['username']; ?>" /><br />
+        <label for="password">Wachtwoord</label>
+        <input type="password" id="password" name="password" value="<?php echo $_POST['password']; ?>" /><br />
+        <input type="submit" name="login" value="Login" />
+    </form>
+    <?php
+// Als er wel op login is gedrukt en de functie var is gevuld
+} else {
+
+    if ($function == "editor") {
+
+        echo "Je bent de redacteur.";
+        $mailsystem->makeSession(true);
+        
+    } else {
+
+        echo "Je bent wel ingelogd, maar ik weet niet als wat!";
+    }
+}
+?>
+</div>
