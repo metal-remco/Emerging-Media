97b2963197ab411b1244412dc8885576478a218b
Remco vanWilgen <remco_vanwilgen@hotmail.com>
2012-05-08 12:15:14 +0200

    md5 + htmlentities & htmlspecialvars toegepast op de data

--- mailsystem/lib/views/subscribe-success.php
+++ mailsystem/lib/views/subscribe-success.php
@@ -5,7 +5,7 @@
     
     	<!-- Todo: Afvangen als er geen Post data is -->
 		<?php // Voeg de gebruiker toe
-        $mailsystem->addSubscriber($_POST['name'], $_POST['email'], $_POST['child_name'], $_POST['child_class'], $_POST['employee_function'], $_POST['employee_class'], $_POST['type']); ?>
+        $mailsystem->addSubscriber(htmlentities(htmlspecialchars($_POST['name'])), htmlentities(htmlspecialchars($_POST['email'])), htmlentities(htmlspecialchars($_POST['child_name'])), htmlentities(htmlspecialchars($_POST['child_class'])), htmlentities(htmlspecialchars($_POST['employee_function'])), htmlentities(htmlspecialchars($_POST['employee_class'])), htmlentities(htmlspecialchars($_POST['type']))); ?>
             
         <!-- Todo: Bevestigingsmail, Oudere nieuwbsrieven bekijken? Klik hier. -->
         <p>De inschrijving is succesvol. U ontvangt vanaf nu onze nieuwsbrief op <strong><?php echo $_POST['email']; ?></strong>.</p>
