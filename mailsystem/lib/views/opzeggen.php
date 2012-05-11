404d920269d48368f09ff619dd656786be46b6e4
Randy van Vugt <randyvanvugt@gmail.com>
2012-05-11 15:21:23 +0200

    Alles aangepast. Helft werkt niet meer. Wat de fak.

--- mailsystem/lib/views/opzeggen.php
+++ mailsystem/lib/views/opzeggen.php
@@ -3,8 +3,7 @@
 
     <div id="unsubscribe" class="shadow">
     
-        <?php $getid = $_GET['id']; // todo: Object gebruiken
-		$mailsystem->removeSubscriber($getid); ?>
+        <?php $mailsystem->removeSubscriber( $mailsystem->getUserID() ); ?>
     
     	<!-- bevestiginsmail -->
         <p>Uw abonnement is succesvol opgezegd, u zult onze nieuwsbrief niet meer ontvangen.</p>
